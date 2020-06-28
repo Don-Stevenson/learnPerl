9569aaa3cc57c8e7bda77bb9eb8bb8df5e0c7db5
Don-Stevenson <dsteven1@gmail.com>
2020-06-28 01:17:54 -0400

    try map

--- variableContext.pl
+++ variableContext.pl
@@ -6,7 +6,10 @@
 $size = @names;
 
 @nums = ( 1, 2, 3, 4 );
+@newNums = map(@nums *2);
+
 
 print "Given names are : @copy\n";
 print "Number of names are : $size\n";
 print "Nums are @nums\n";
+print "Newnums are @newNums\n";
