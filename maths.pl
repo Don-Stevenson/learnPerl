5ec1ecefea87b075e3329bbb80bbceffa0f0dfa9
Don-Stevenson <dsteven1@gmail.com>
2020-06-27 20:01:42 -0400

    add circle area

--- maths.pl
+++ maths.pl
@@ -9,4 +9,10 @@ use warnings;
   $width;
 
 
-print "area is length $length multiplied by $length. The area is $area\n";
+print "area is length $length multiplied by $width. The area is $area\n";
+
+$radius = 5;
+$PI = 3.14159256;
+$circleArea = $PI * $radius * $radius;
+
+print "Circle area is Pi($PI) R($radius)^2. The area is $circleArea\n";
