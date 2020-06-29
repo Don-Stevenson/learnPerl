eb8ee36e28938fe0977d9275917695dd8bd94c04
Don-Stevenson <dsteven1@gmail.com>
2020-06-29 12:44:10 -0400

    remove newnums map

--- variableContext.pl
+++ variableContext.pl
@@ -6,10 +6,7 @@
 $size = @names;
 
 @nums = ( 1, 2, 3, 4 );
-@newNums = map @nums(x => x * 2);
-
 
 print "Given names are : @copy\n";
 print "Number of names are : $size\n";
 print "Nums are @nums\n";
-print "Newnums are @newNums\n";
