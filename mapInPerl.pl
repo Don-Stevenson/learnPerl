30731040055064069c24fb83f30b047ab1457fb9
Don-Stevenson <dsteven1@gmail.com>
2020-06-29 13:06:19 -0400

    using sqaures greater than 5

--- mapInPerl.pl
+++ mapInPerl.pl
@@ -1,11 +1,16 @@
 #!/usr/bin/perl
 
-@nums = ( 1, 2, 3, 4);
+@nums = ( 1, 2, 3, 4, 5, 6);
 
 
 # maps over nums and squares each item by $_ * $_ 
 @squares = map { $_ * $_ } @nums;
 
+# only returns a sqaures a number to the new array if it's greater than 5
+@squaresGreaterThanFive = map { $_ > 5 ? ($_ * $_) : () } @nums;
+
 print "@nums\n";
 
 print "@squares \n";
+
+print "@squaresGreaterThanFive \n";
