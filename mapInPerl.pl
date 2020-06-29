88af6027cbd5fbdd6c32d6710b789a3a0c79c181
Don-Stevenson <dsteven1@gmail.com>
2020-06-29 13:27:14 -0400

    add nums > five

--- mapInPerl.pl
+++ mapInPerl.pl
@@ -2,15 +2,20 @@
 
 @nums = ( 1, 2, 3, 4, 5, 6);
 
-
 # maps over nums and squares each item by $_ * $_ 
 @squares = map { $_ * $_ } @nums;
 
-# only returns a sqaures a number to the new array if it's greater than 5
+#only returns a number to the new array if it greater than five
+@numsGreaterThanFive = map { $_ > 5 ? ($_) : () }@nums;
+
+
+# only returns a sqaure a number to the new array if it's greater than 5
 @squaresGreaterThanFive = map { $_ > 5 ? ($_ * $_) : () } @nums;
 
 print "@nums\n";
 
 print "@squares \n";
+
+print "@numsGreaterThanFive \n";
 
 print "@squaresGreaterThanFive \n";
