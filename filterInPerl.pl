897b75ec6cda569ea92f325aa2245ff583280780
Don-Stevenson <dsteven1@gmail.com>
2020-07-02 00:13:10 -0400

    trying map with strings in an array

--- filterInPerl.pl
+++ filterInPerl.pl
@@ -3,8 +3,12 @@
 #learning filter in perl
 #***********************
 
-@nums= (1, 2, 3, 4, 5, 6, 7, 8);
+@nums = (1, 2, 3, 4, 5, 6, 7, 8);
+@letters = ("a","b","c","d","e","f","g");
 
 @numsFiltered =  map { $_ > 1 ? $_ : () }@nums;
 
+@lettersFiltered =  map { $_ = "a" ? $_ : () }@letters;
+
 print "@numsFiltered \n"
+print "@lettersFiltered \n"
